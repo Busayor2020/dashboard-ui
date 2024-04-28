95ec7427ebf9d782d455f9b50d8b131332776f89
Victor Busayor <ogunrinolavictor2015@gmail.com>
2024-04-28 13:58:42 +0100

    build

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import { FaSearch, FaBell, FaUser } from 'react-icons/fa';
-import Avatar from '@mui/material/Avatar';
+
 
 const Navbar: React.FC = () => { 
  return (
--- src/components/charts/CurveChart.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CurveChart = () => {
-  return (
-    <div>CurveChart</div>
-  )
-}
-
-export default CurveChart
